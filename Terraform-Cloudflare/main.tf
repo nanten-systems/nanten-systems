23d0e125d2e7cfec7556459d543a5fb1e4e2161e
nanten-systems <263316179+nanten-systems@users.noreply.github.com>
2026-03-21 15:32:16 +0900

    CloudflareをTerraformで操作できるか試した

---
+++ Terraform-Cloudflare/main.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 5"
+    }
+  }
+}
+
+provider "cloudflare" {
+  # API token will be read from CLOUDFLARE_API_TOKEN environment variable
+}
+
+variable "zone_id" {
+  description = "Cloudflare Zone ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "account_id" {
+  description = "Cloudflare Account ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "domain" {
+  description = "Domain name"
+  type        = string
+  default     = "example.com"
+}
+
+resource "cloudflare_dns_record" "www" {
+  zone_id = var.zone_id
+  name    = "www"
+  content = "203.0.113.10"
+  type    = "A"
+  ttl     = 1
+  proxied = true
+  comment = "Domain verification record"
+}
